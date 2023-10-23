585fdfe463396bc00179c667e0dd3c0100ac4203
Matheus-Marques-de-oliveira <130381677+Matheus-Marques-de-oliveira@users.noreply.github.com>
2023-10-23 08:57:10 -0300

    Add files via upload

---
+++ prova_front_end_3tri/calculo.js
@@ -0,0 +1,9 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+
+let Calcular = document.mostraresultado ("Resultado: a idade do cachorrinho é" + idadecanina);
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+}
